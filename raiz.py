d98f50845524927526eeaa42998b5f8cdcc994c3
Alan Parizek <alannicolasparizekqgmail.com>
2022-11-04 17:19:00 -0300

    raiz y division con manejo de errores

--- raiz.py
+++ raiz.py
@@ -1,4 +1,18 @@
 import math
 
 def raizCuadrada(numero):
-    return math.sqrt(numero)
+    numero = -1
+    try:
+        numero = math.sqrt(numero)
+    except ValueError:
+        print("no se puede hacer al raiz de un numero negativo")
+
+    return numero
+
+def division(numero1, numero2):
+    try:
+        numero = numero1/numero2
+    except ZeroDivisionError:
+        print("intentaste dividir por 0")
+
+    return numero
